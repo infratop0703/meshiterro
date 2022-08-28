c0a29dd1371b78fc5035080e44458979e5edf58e
EC2 Default User <ec2-user@ip-172-31-14-67.ec2.internal>
2022-08-28 15:18:34 +0000

    [Update]19-いいね機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -6,9 +6,10 @@ Rails.application.routes.draw do
 
   resources :post_images, only: [:new, :create, :index, :show, :destroy
   ] do
+    resource :favorites, only: [:create, :destroy]
     resources :post_comments, only: [:create,:destroy]
   end
-  
+
   resources :users, only:[:show,:edit,:update]
 
 
